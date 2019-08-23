ae57e0ebefc5e662334af5bd5474b9ff59ab169b
Elwin Tamminga <elwintamminga@gmail.com>
2019-08-24 01:53:17 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo "[*] Connecting to device"
+if ! timeout 5 adb $1 wait-for-device; then
+	echo "Could not connect with ADB" >&2
+	exit 1
+fi
+
+echo "[*] Launching Termux"
+adb $1 shell am start -n com.termux/.app.TermuxActivity > /dev/null &&
+sleep 5 &&
+
+echo "[*] Setting up telnet connection" &&
+adb $1 shell input text "termux-setup-storage\ \&\&\ busybox\ telnetd\ -Fb\ 127.0.0.1:8023\;\ exit\ 0" &&
+adb $1 shell input keyevent 66 &&
+adb $1 forward tcp:8023 tcp:8023
+
+until echo exit | nc -T localhost 8023 > /dev/null 2>&1; do
+	sleep 0.1 && ((i++))
+	if [ $i -ge 100 ]; then
+		echo "Could not connect to telnet with netcat (screen locked?)" >&2
+		exit 1
+	fi
+done
+
+quit () {
+	echo "killall busybox || logout" | nc -T localhost 8023 > /dev/null
+	exit $1
+}
+
+cmd () {
+	OUT=$(echo "$1 && echo; logout" | nc -T localhost 8023 | sed -u '1,/$ /d' | tee /dev/tty | tail -n1)
+	if [ ${#OUT} -gt 1 ]; then
+		quit 1
+	fi
+}
+
+echo "[*] Checking storage permissions"
+cmd "until [ -r ~/storage/downloads ]; do sleep .1; done"
+
+echo "[*] Installing mpv"
+cmd "apt -y update && apt -y upgrade && apt -y install mpv && mkdir -p ~/.config/mpv && echo loop-playlist > ~/.config/mpv/mpv.conf"
+
+echo "[*] Installing youtube-dl"
+cmd "apt -y install python && pip --disable-pip-version-check install youtube-dl"
+
+echo "Done"
+quit 0
